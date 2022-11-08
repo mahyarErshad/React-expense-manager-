3a013e9a8e2025c773ad98f9bc840ac273c40b1e
mahyarErshad <mahyar.ershad92@gmail.com>
2022-11-08 13:12:11 +0330

    Html elements have initialized

---
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>March 28th 2022</h2>
+      <div>
+        <h2>Car insurance</h2>
+        <h2>$294.25</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
