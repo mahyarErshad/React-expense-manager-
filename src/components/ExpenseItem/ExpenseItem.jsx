9f44fac47a57b4535274edae51b91cbfadd45c19
mahyarErshad <mahyar.ershad92@gmail.com>
2022-11-08 15:00:51 +0330

    Added a wrapper

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -7,15 +7,17 @@ function ExpenseItem() {
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const year = date.getFullYear();
   return (
-    <div className="expense-item">
-      <div className="expense-date">
-        <div className="expense-date__month">{month}</div>
-        <div className="expense-year">{year}</div>
-        <div className="expense-day">{day}</div>
-      </div>
-      <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <h3 className="expense-item__price">$294.25</h3>
+    <div className="expenses">
+      <div className="expense-item">
+        <div className="expense-date">
+          <div className="expense-date__month">{month}</div>
+          <div className="expense-year">{year}</div>
+          <div className="expense-day">{day}</div>
+        </div>
+        <div className="expense-item__description">
+          <h2>Car insurance</h2>
+          <h3 className="expense-item__price">$294.25</h3>
+        </div>
       </div>
     </div>
   );
