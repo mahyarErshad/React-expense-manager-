06788c863ecc6701184b5d507fd0e07a749b1d19
mahyarErshad <mahyar.ershad92@gmail.com>
2022-11-08 13:14:49 +0330

    Giving items classnames

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
   return (
-    <div>
-      <h2>March 28th 2022</h2>
-      <div>
+    <div className="expense-item">
+      <h3>March 28th 2022</h3>
+      <div className="expense-item__description">
         <h2>Car insurance</h2>
-        <h2>$294.25</h2>
+        <h3 className="expense-item__price">$294.25</h3>
       </div>
     </div>
   );
