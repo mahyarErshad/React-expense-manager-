d1f501a7acde776150b634cade9f3e7cf17f71e9
mahyarErshad <mahyar.ershad92@gmail.com>
2022-11-09 09:58:58 +0330

    Imported ExpenseForm

--- src/components/ExpenseMonitor/ExpenseSection/ExpenseSection.jsx
+++ src/components/ExpenseMonitor/ExpenseSection/ExpenseSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ExpenseForm from '../ExpenseForm/ExpenseForm'
 import "./ExpenseSection.css"
 
 function ExpenseSection() {
   return (
-    <div>ExpenseSection</div>
+    <div className='new-expense'>
+      <ExpenseForm />
+    </div>
   )
 }
 
