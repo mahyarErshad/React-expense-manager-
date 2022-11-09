fe7280ff447321194ba91aea1f6858a7a0ce6f79
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-09 13:51:42 +0330

    Imported ExpenseFilter

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ExpenseCard from "../ExpenseCard/ExpenseCard";
+import ExpenseFilter from "../ExpenseMonitor/ExpenseFilter/ExpenseFilter";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
   return (
     <div className="expenses">
+      <ExpenseFilter />
       <ExpenseCard />
     </div>
   );
