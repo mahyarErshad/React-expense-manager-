bf6e29f90b2f08238ccf6eb863d5e3c3a4918d5e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-09 13:52:12 +0330

    Added a label

--- src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
+++ src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
@@ -5,6 +5,7 @@ function ExpenseFilter() {
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
+      <label>Filter by year</label>
         <select>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
