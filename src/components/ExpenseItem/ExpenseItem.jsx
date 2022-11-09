a02227e4c341298cdfd4bf6d5cc1ca5b36dddafb
mahyarErshad <mahyar.ershad92@gmail.com>
2022-11-09 09:53:17 +0330

    Imported ExpenseCard

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ExpenseCard from "../ExpenseCard/ExpenseCard";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
@@ -8,7 +9,7 @@ function ExpenseItem() {
   const year = date.getFullYear();
   return (
     <div className="expenses">
-
+    <ExpenseCard />
     </div>
   );
 }
