2a3e2f66d8fac3e045cc61208bdda1d4e4715d8c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-09 13:49:21 +0330

    Added the elements

---
+++ src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ExpenseFilter() {
+  return (
+    <div>
+        <div>
+            <select>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+            </select>
+        </div>
+    </div>
+  )
+}
+
+export default ExpenseFilter
