07e0f9ebab3a473b8e5ffd9bac6edf9447481e8c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-09 15:25:40 +0330

    Defined a state for expenses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import ExpenseSection from "./components/ExpenseMonitor/ExpenseSection/ExpenseSection";
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import { useState } from "react";
 
 function App() {
+  const [expenses , setExpense] = useState()
   return (
     <div>
       <ExpenseSection />
