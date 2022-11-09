dbb904b80f1727e603e2abd0701d0847a1394dd8
mahyarErshad <mahyar.ershad92@gmail.com>
2022-11-09 09:53:42 +0330

    Moved Card variables to it's component

--- src/components/ExpenseCard/ExpenseCard.jsx
+++ src/components/ExpenseCard/ExpenseCard.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "../ExpenseItem/ExpenseItem.css";
 
 function ExpenseCard() {
+  const date = new Date();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
   return (
     <div className="expense-item">
       <div className="expense-date">
--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -3,13 +3,9 @@ import ExpenseCard from "../ExpenseCard/ExpenseCard";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  const date = new Date();
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
   return (
     <div className="expenses">
-    <ExpenseCard />
+      <ExpenseCard />
     </div>
   );
 }
