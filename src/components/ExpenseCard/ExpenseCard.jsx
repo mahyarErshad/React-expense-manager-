013ec2fd07007d395bf332a87ece48ea5ea0203c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-09 15:36:31 +0330

    Card is dynamic using props

--- src/components/ExpenseCard/ExpenseCard.jsx
+++ src/components/ExpenseCard/ExpenseCard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "../ExpenseItem/ExpenseItem.css";
 
-function ExpenseCard() {
-  const date = new Date();
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
+function ExpenseCard({expenses}) {
+  const month = expenses.date.toLocaleString("en-US", { month: "long" });
+  const day = expenses.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = expenses.date.getFullYear();
   return (
     <div className="expense-item">
       <div className="expense-date">
@@ -14,8 +13,8 @@ function ExpenseCard() {
         <div className="expense-day">{day}</div>
       </div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <h3 className="expense-item__price">$294.25</h3>
+        <h2>{expenses.title}</h2>
+        <h3 className="expense-item__price">{expenses.amount}</h3>
       </div>
     </div>
   );
