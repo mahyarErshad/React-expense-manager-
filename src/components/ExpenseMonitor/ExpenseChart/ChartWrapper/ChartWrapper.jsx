0baf3fc0344eb1edce20d46a48ea8e4fb384ad29
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 11:33:47 +0330

    Created dataPoints array

--- src/components/ExpenseMonitor/ExpenseChart/ChartWrapper/ChartWrapper.jsx
+++ src/components/ExpenseMonitor/ExpenseChart/ChartWrapper/ChartWrapper.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import Chart from '../Chart/Chart'
+import React from "react";
+import Chart from "../Chart/Chart";
 
 function ChartWrapper() {
+  const dataPoints = [
+    {
+      label: "January",
+      value: 0,
+    },
+    {
+      label: "February",
+      value: 0,
+    },
+    {
+      label: "March",
+      value: 0,
+    },
+    {
+      label: "April",
+      value: 0,
+    },
+    {
+      label: "May",
+      value: 0,
+    },
+    {
+      label: "June",
+      value: 0,
+    },
+    {
+      label: "July",
+      value: 0,
+    },
+    {
+      label: "August",
+      value: 0,
+    },
+    {
+      label: "September",
+      value: 0,
+    },
+    {
+      label: "October",
+      value: 0,
+    },
+    {
+      label: "November",
+      value: 0,
+    },
+    {
+      label: "December",
+      value: 0,
+    },
+  ];
   return (
     <>
-        <Chart />
+      <Chart dataPoints={dataPoints} />
     </>
-  )
+  );
 }
 
-export default ChartWrapper
+export default ChartWrapper;
