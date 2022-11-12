682a1893afff5dc82d4c15d832cd7e656e06290e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 11:23:36 +0330

    Initialized the Chart component

--- src/components/ExpenseMonitor/ExpenseChart/Chart/Chart.jsx
+++ src/components/ExpenseMonitor/ExpenseChart/Chart/Chart.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import "./Chart.css"
-import ChartBar from "../ChartBar/ChartBar"
+import React from "react";
+import "./Chart.css";
+import ChartBar from "../ChartBar/ChartBar";
 
 function Chart(props) {
   return (
-    <div className='chart'>
-        {props.dataPoints.map((dataPoint)=>{
-            <ChartBar />
-        })}
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => {
+        <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label} />;
+      })}
     </div>
-  )
+  );
 }
 
-export default Chart
+export default Chart;
