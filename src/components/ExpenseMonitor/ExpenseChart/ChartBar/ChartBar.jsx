e64a13c4c241bd79f808567086191f95f10162cb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 11:03:23 +0330

    ChartBar component is done

--- src/components/ExpenseMonitor/ExpenseChart/ChartBar/ChartBar.jsx
+++ src/components/ExpenseMonitor/ExpenseChart/ChartBar/ChartBar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./ChartBar.css";
 
 function ChartBar(props) {
+  let barFieldHeight = "0%";
+  if (props.max > 0) {
+    barFieldHeight = Math.round((props.value / props.max) * 100) + "%";
+  }
   return (
-    <div>ChartBar</div>
-  )
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{ height: barFieldHeight }}></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
 }
 
-export default ChartBar
+export default ChartBar;
