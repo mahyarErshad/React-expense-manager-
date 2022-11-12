716075e2ff9d2d0d58dc21461fc3912d0f912ade
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 08:56:03 +0330

    Passing value to filter component

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseCard from "../ExpenseCard/ExpenseCard";
 import ExpenseFilter from "../ExpenseMonitor/ExpenseFilter/ExpenseFilter";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("2020")
+
   return (
     <div className="expenses">
-      <ExpenseFilter />
+      <ExpenseFilter value={filteredYear} />
       {expenses.length &&
         expenses.map((expense) => {
           return <ExpenseCard key={expense.id} expenses={expense} />;
