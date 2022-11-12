58eb3e5018a8861e99290ee3971306be169c93ae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 10:24:00 +0330

    Defined a new state

--- src/components/ExpenseMonitor/ExpenseSection/ExpenseSection.jsx
+++ src/components/ExpenseMonitor/ExpenseSection/ExpenseSection.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseSection.css";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 
 function ExpenseSection(props) {
+  const [addANewExpense , setAddAnewExpense] = useState()
   return (
     <div className="new-expense">
     <button>Add a new expense</button>
