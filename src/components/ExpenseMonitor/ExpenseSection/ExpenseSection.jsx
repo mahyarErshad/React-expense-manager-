19972d50f9bf71ba546c4ba6fdeded389a2bf3a6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 10:24:58 +0330

    Conditionally rendering the form

--- src/components/ExpenseMonitor/ExpenseSection/ExpenseSection.jsx
+++ src/components/ExpenseMonitor/ExpenseSection/ExpenseSection.jsx
@@ -3,13 +3,8 @@ import "./ExpenseSection.css";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 
 function ExpenseSection(props) {
-  const [addANewExpense , setAddAnewExpense] = useState()
-  return (
-    <div className="new-expense">
-    <button>Add a new expense</button>
-      <ExpenseForm setExpenses={props.setExpenses} />
-    </div>
-  );
+  const [addANewExpense, setAddAnewExpense] = useState();
+  return <div className="new-expense">{addANewExpense ? <ExpenseForm setExpenses={props.setExpenses} /> : <button>Add a new expense</button>}</div>;
 }
 
 export default ExpenseSection;
