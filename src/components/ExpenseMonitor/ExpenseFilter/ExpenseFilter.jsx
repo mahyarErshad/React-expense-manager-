a80b04de89bd608d4a61aaac4d5803d9659f9e98
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 08:58:00 +0330

    Setting value using setFilteredYear

--- src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
+++ src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./ExpenseFilter.css";
 
-function ExpenseFilter({value}) {
+function ExpenseFilter({value, setFilteredYear}) {
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
       <label>Filter by year</label>
-        <select value={value}>
+        <select onChange={(e)=>setFilteredYear(e.target.value)} value={value}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
