b6261e11d8a6ea033d328978823e2de53a299cf0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 10:34:40 +0330

    Added more years

--- src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
+++ src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
@@ -7,6 +7,9 @@ function ExpenseFilter({value, setSelectedYear}) {
       <div className="expenses-filter__control">
       <label>Filter by year</label>
         <select onChange={(e)=>setSelectedYear(e.target.value)} value={value}>
+          <option value="2025">2025</option>
+          <option value="2024">2024</option>
+          <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
