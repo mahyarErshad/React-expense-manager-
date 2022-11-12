f4d40687fa89394fa0f3de6942f25b5927353eea
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 08:56:28 +0330

    Catching Value from parent

--- src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
+++ src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./ExpenseFilter.css";
 
-function ExpenseFilter() {
+function ExpenseFilter({value}) {
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
       <label>Filter by year</label>
-        <select>
+        <select value={value}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
