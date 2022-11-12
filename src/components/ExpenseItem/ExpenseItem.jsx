6b6aba8575bfcf8dd9512e70a1ed3d760eb0275d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 08:59:57 +0330

    Renamed the state

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -4,11 +4,13 @@ import ExpenseFilter from "../ExpenseMonitor/ExpenseFilter/ExpenseFilter";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expenses }) {
-  const [filteredYear, setFilteredYear] = useState("2020")
-
+  const [selectedYear, setSelectedYear] = useState("2020")
+  const filteredExpenses = () => {
+    selectedYear.filter
+  }
   return (
     <div className="expenses">
-      <ExpenseFilter value={filteredYear} setFilteredYear={setFilteredYear} />
+      <ExpenseFilter value={selectedYear} setSelectedYear={setSelectedYear} />
       {expenses.length &&
         expenses.map((expense) => {
           return <ExpenseCard key={expense.id} expenses={expense} />;
--- src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
+++ src/components/ExpenseMonitor/ExpenseFilter/ExpenseFilter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./ExpenseFilter.css";
 
-function ExpenseFilter({value, setFilteredYear}) {
+function ExpenseFilter({value, setSelectedYear}) {
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
       <label>Filter by year</label>
-        <select onChange={(e)=>setFilteredYear(e.target.value)} value={value}>
+        <select onChange={(e)=>setSelectedYear(e.target.value)} value={value}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
