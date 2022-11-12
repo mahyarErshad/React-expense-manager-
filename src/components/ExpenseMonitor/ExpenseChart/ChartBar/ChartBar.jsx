46cd9ce90b313dba861a3be734f0c386b8765f5a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-12 14:08:17 +0330

    Background color is based on the height

--- src/components/ExpenseMonitor/ExpenseChart/ChartBar/ChartBar.jsx
+++ src/components/ExpenseMonitor/ExpenseChart/ChartBar/ChartBar.jsx
@@ -3,13 +3,25 @@ import "./ChartBar.css";
 
 function ChartBar(props) {
   let barFieldHeight = "0%";
+  const heightPercentage = Math.round((props.value / props.maxValue) * 100);
   if (props.maxValue > 0) {
-    barFieldHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+    barFieldHeight = heightPercentage + "%";
   }
+  let backgroundColor;
+  if (heightPercentage < 30) {
+    backgroundColor = "#248632";
+  } else if (heightPercentage >= 30 && heightPercentage < 50) {
+    backgroundColor = "#ffc007";
+  } else if (heightPercentage >= 50 && heightPercentage < 80) {
+    backgroundColor = "#f1601f";
+  } else {
+    backgroundColor = "#dc3544";
+  }
+
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill" style={{ height: barFieldHeight }}></div>
+        <div className="chart-bar__fill" style={{ height: barFieldHeight, backgroundColor }}></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
